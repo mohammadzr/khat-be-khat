8476fa8bfcab98f642a412fa3a10cec9469ec91c
Mohammad <codev.bussines@gmail.com>
2025-03-02 19:44:46 +0330

    Revert "feat/addin auth system - ai"
    This reverts commit 5190c4775b86b9e45818b14cb43f8bfe12cd655e.

--- src/components/layout/GuestLayout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-
-interface GuestLayoutProps {
-  children: React.ReactNode;
-}
-
-const GuestLayout: React.FC<Guest
